34b54352a52074699dfc9e939b74872730509bf9
andela-eakinyele <emmanuel.akinyele@andela.com>
2015-12-18 21:36:26 +0100

    add test spec, create app folder

---
+++ spec/mockMethods.js
@@ -0,0 +1,50 @@
+var promise = require("bluebird");
+var _ = require("lodash");
+var ModelFunc = require("./../app/controllers");
+var User = require("./../app/models/user");
+var Doc = require("./../app/models/document");
+var Role = require("./../app/models/role");
+
+exports.roleMock = function (testRoles) {
+  return promise.mapSeries(testRoles, function (role) {
+    return ModelFunc.roleFunc.createRole(new Array(role));
+  });
+};
+
+exports.userMock = function (testUsers) {
+  return promise.mapSeries(_.values(testUsers), function (userdata) {
+    return ModelFunc.userFunc.createUser(userdata);
+  });
+};
+
+exports.docMock = function (testDocs) {
+  return promise.mapSeries(_.values(testDocs), function (doc) {
+    return ModelFunc.docFunc.createDoc(doc);
+  });
+};
+
+exports.parseData = function (keys, _data) {
+  return _.zipObject(keys, _data);
+};
+
+exports.deleteModels = function (cb) {
+  User.remove().exec(function (err) {
+    if (err) {
+      console.log("Users not removed");
+      return;
+    }
+    Role.remove().exec(function (err) {
+      if (err) {
+        console.log("Roles not removed");
+        return;
+      }
+      Doc.remove().exec(function (err) {
+        if (err) {
+          console.log("Users not removed");
+          return;
+        }
+        cb();
+      });
+    });
+  });
+};
