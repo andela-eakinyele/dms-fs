aae33055f4d3aeff5f2c2647611a5e8a6d19a1dd
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-01-30 15:24:18 +0100

    Update dashboard and welcome page
    [Finishes #112226751]
    
    Refactor util service
    Redefine welcome page view and controllers

---
+++ app/src/controllers/home.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+  angular.module('prodocs.controllers')
+    .controller('StartPageCtrl', ['$mdMedia', '$rootScope', '$scope',
+      '$state', '$timeout',
+      function($mdMedia, $rootScope, $scope, $state, $timeout) {
+
+        $scope.showButton = true;
+
+        $timeout(function() {
+          $scope.$watch(function() {
+            return $state.current.name;
+          }, function(name) {
+            if (name === 'home.features') {
+              $scope.showButton = true;
+            } else {
+              $scope.showButton = false;
+            }
+          });
+        }, 0.5);
+      }
+    ]);
+})();
