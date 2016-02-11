713e6cf60d2ba7c31e88a174a4b188239025e915
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-02-11 11:20:24 +0100

    Admin Role Action
    [Delivers #113519509]
    
    - Update create an delete button actions

--- server/controllers/roleMethods.js
+++ server/controllers/roleMethods.js
@@ -4,6 +4,7 @@
   var Doc = require('./../models/document');
   var group = require('./groupMethods');
   var Group = require('./../models/group');
+  var _ = require('lodash');
 
   var cm = require('./helpers');
 
@@ -22,19 +23,54 @@
   }
 
   var roleFunctions = {
+    bulkCreate: function(req, res) {
+      var groupid = parseInt(req.headers.groupid);
+      var titles = _.pluck(req.body, 'title');
+      var bulkData = req.body;
+      Role.find({
+          groupId: [groupid]
+        })
+        .where('title').in(titles)
+        .exec(function(err, roles) {
+          if (roles.length > 0) {
+            res.status(400).json(roles);
+          } else {
+            Role.getMaxId().then(function(data) {
+              if (data.length) {
+                var nextId = cm.getNextId(data);
+              }
+              bulkData = _.map(bulkData, function(a, index) {
+                a._id = nextId + index;
+                return a;
+              });
+              Role.collection.insert(req.body, function(err, inserted) {
+                if (err) {
+                  res.status(400).json(err);
+                } else {
+                  res.status(201).json(inserted);
+                }
+              });
+            }).catch(function(err) {
+              cm.resdberrors(res, 'querying database', err);
+            });
+          }
+        });
 
+    },
     create: function(req, res) {
+
+      req.body.groupId = [parseInt(req.headers.groupid)];
       // query for existing role
       var query = Role.find({
         title: req.body.title,
-        groupId: [req.body.groupId]
+        groupId: [parseInt(req.headers.groupid)]
       });
       // create role or return existing role
       cm.gCreate('Roles', req.body, Role, query)
         .then(function(role) {
           // query for group
           var query = {
-            _id: req.body.groupId
+            _id: req.headers.groupid
           };
 
           // retrieve group and update with role
@@ -43,13 +79,13 @@
               gr.roles.push(role.data);
 
               // query for updating group data with new role
-              var query2 = Group.findByIdAndUpdate(req.body.groupId,
+              var query2 = Group.findByIdAndUpdate(req.headers.groupid,
                 gr, {
                   new: true
                 });
 
               // update group data
-              cm.gUpdate('Groups', req.body.groupId, query2)
+              cm.gUpdate('Groups', req.headers.groupid, query2)
                 .then(function() {
                   // return status of created role
                   res.status(role.status).json(role);
@@ -96,13 +132,13 @@
 
     all: function(req, res) {
       var query = Role.find({
-        groupId: req.headers.groupid
+        groupId: [parseInt(req.headers.groupid)]
       });
       cm.gGetAll('Roles', query)
         .then(function(result) {
-          res.status(result.status).json(result);
+          res.status(result.status).json(result.data);
         }).catch(function(err) {
-          res.status(err.status).json(err);
+          res.status(err.status).json(err.error);
         });
     },
 
@@ -112,9 +148,9 @@
       });
       cm.gGetOne('Roles', query, req.params.id)
         .then(function(result) {
-          res.status(result.status).json(result);
+          res.status(result.status).json(result.data);
         }).catch(function(err) {
-          res.status(err.status).json(err);
+          res.status(err.status).json(err.error);
         });
     },
 
@@ -125,9 +161,9 @@
         });
       cm.gUpdate('Roles', req.params.id, query)
         .then(function(result) {
-          res.status(result.status).json(result);
+          res.status(result.status).json(result.data);
         }).catch(function(err) {
-          res.status(err.status).json(err);
+          res.status(err.status).json(err.error);
         });
     },
 
@@ -135,12 +171,23 @@
       var query = Role.findByIdAndRemove(req.params.id);
       cm.gDelete('Roles', query, req.params.id)
         .then(function(result) {
-          res.status(result.status).json(result);
+          res.status(result.status).json(result.data);
         }).catch(function(err) {
-          res.status(err.status).json(err);
+          res.status(err.status).json(err.error);
         });
     },
 
+    bulkDelete: function(req, res) {
+      Role.remove({})
+        .where('_id').in(req.body)
+        .exec(function(err, deleted) {
+          if (err) {
+            res.status(500).json(err.error);
+          } else {
+            res.status(200).json(deleted);
+          }
+        });
+    },
 
     getDocsByRole: function(req, res) {
       Doc.getDocsByRole(req.params.id,
