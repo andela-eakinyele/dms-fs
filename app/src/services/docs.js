973ff694326d6c974fa3c3fa3c72a668da115f9a
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-02-14 08:16:01 +0100

    Fix Houndci error
    [Fixes #113608573]
    
    - Fix lint errors

--- app/src/services/docs.js
+++ app/src/services/docs.js
@@ -2,26 +2,28 @@
   'use strict';
 
   angular.module('prodocs.services')
-    .factory('Docs', ['$resource', '$http', function docFactory($resource, $http) {
-      var obj = $resource('/api/documents/:id', {
-        id: '@id'
-      }, {
-        update: {
-          // this method issues a PUT request
-          method: 'PUT'
-        }
-      }, {
-        stripTrailingSlashes: false
-      });
+    .factory('Docs', ['$resource', '$http',
+      function docFactory($resource, $http) {
+        var obj = $resource('/api/documents/:id', {
+          id: '@id'
+        }, {
+          update: {
+            // this method issues a PUT request
+            method: 'PUT'
+          }
+        }, {
+          stripTrailingSlashes: false
+        });
 
-      obj.getUserDocs = function(id, cb) {
-        return $http.get('/api/users/' + id + '/documents')
-          .then(function(data) {
-            cb(null, data);
-          }, function(err) {
-            cb(err, null);
-          });
-      };
-      return obj;
-    }]);
+        obj.getUserDocs = function(id, cb) {
+          return $http.get('/api/users/' + id + '/documents')
+            .then(function(data) {
+              cb(null, data);
+            }, function(err) {
+              cb(err, null);
+            });
+        };
+        return obj;
+      }
+    ]);
 })();
