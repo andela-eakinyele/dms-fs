1883b15651dc5fe51866d4e2d7c0e65df3a2b8c6
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-02-21 12:28:43 +0100

    Fix Deployment access to heroku
    [Starts #114087525]
    
    - Fix dir path for static files

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@
 
   app.use('/api', apiRoutes);
 
-  app.use(express.static(path.join(__dirname, './../public')));
+  app.use(express.static(path.join(__dirname, './public')));
 
   app.get('/*', function(req, res) {
     res.sendFile('index.html', {
