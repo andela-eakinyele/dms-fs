a1bd9774c95c4d1c3f6158fb035025b4d3563d93
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-02-22 04:28:05 +0100

    Fix hook errors on server tests on circleci Refers to [#114172209]
    - Use async lib, remove bluebird

--- tests/server/mockMethods.js
+++ tests/server/mockMethods.js
@@ -1,43 +1,87 @@
 (function() {
   'use strict';
-  var promise = require('bluebird');
   var bcrypt = require('bcrypt-nodejs');
   var _ = require('lodash');
   var User = require('./../../server/models/user');
   var Doc = require('./../../server/models/document');
   var Role = require('./../../server/models/role');
   var Group = require('./../../server/models/group');
+  var _async = require('async');
+
 
   exports.seedCreate = function(model, keys, seedData, minId) {
-    keys.push('_id');
-    return promise.mapSeries(_.values(seedData), function(data, index) {
-      data.push(minId + index);
-      var _data = _.zipObject(keys, data);
-      if (_data.password) {
-        _data.password = bcrypt.hashSync(_data.password);
-      }
-      if (_data.passphrase) {
-        _data.passphrase = bcrypt.hashSync(_data.passphrase);
-      }
-      return model.create(_data)
-        .then(function(created) {
-          return created;
-        }, function(err) {
-          return err;
+    return new Promise(function(resolve, reject) {
+      _async.waterfall([
+          function(done) {
+            var seedResult = [];
+
+            keys.push('_id');
+            _async.forEachOf(_.values(seedData), function(data, index, cb) {
+              data.push(minId + index);
+              var _data = _.zipObject(keys, data);
+              if (_data.password) {
+                _data.password = bcrypt.hashSync(_data.password);
+              }
+              if (_data.passphrase) {
+                _data.passphrase = bcrypt.hashSync(_data.passphrase);
+              }
+              model.create(_data)
+                .then(function(created) {
+                  seedResult.push(created);
+                  cb(null, true);
+                }, function(err) {
+                  cb(err, null);
+                });
+            }, function(err) {
+              if (err) {
+                done(err, null);
+              } else {
+                done(null, seedResult);
+              }
+            });
+          }
+        ],
+        function(err, res) {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
         });
     });
   };
 
   exports.seedUpdate = function(model, seedData, seedIds) {
-    return promise.mapSeries(seedData, function(data, index) {
-      return model.findByIdAndUpdate(seedIds[index],
-          data, {
-            new: true
-          })
-        .then(function(updated) {
-          return updated;
-        }, function(err) {
-          return err;
+    return new Promise(function(resolve, reject) {
+      _async.waterfall([
+          function(done) {
+            var seedResult = [];
+
+            _async.forEachOf(seedData, function(data, index, cb) {
+              model.findByIdAndUpdate(seedIds[index],
+                  data, {
+                    new: true
+                  })
+                .then(function(updated) {
+                  seedResult.push(updated);
+                  cb(null, true);
+
+                }, function(err) {
+                  cb(err, null);
+                });
+            }, function(err) {
+              if (err) {
+                done(err, null);
+              } else {
+                done(null, seedResult);
+              }
+            });
+          }
+        ],
+        function(err, res) {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
         });
     });
   };
